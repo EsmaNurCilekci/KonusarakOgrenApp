c826823061e585c3149edffa7006ad5b08a06cc3
EsmaNurCilekci <esmacilekci@gmail.com>
2022-08-12 08:51:08 +0300

    Add project files.

---
+++ KonusarakOgren.Core/Services/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using KonusarakOgren.Core.DTOs;
+using KonusarakOgren.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonusarakOgren.Core.Services
+{
+    public interface IProductService :IService<Product>
+    {
+        Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory();
+
+    }
+}
---
+++ KonusarakOgren.Service/Mapping/MapProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using KonusarakOgren.Core.DTOs;
+using KonusarakOgren.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonusarakOgren.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+            CreateMap<Product, ProductWithCategoryDto>();
+            CreateMap<Category, CategoryWithProductsDto>();
+        }
+
+
+    }
+}
